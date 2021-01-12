7e78d25a015d55f020cda0bdb6055de1769d6fea
Trọng Hiếu <tronghieu60s@gmail.com>
2021-01-12 08:39:33 +0700

    word details

--- components/TabPractice/WordDetails/index.tsx
+++ components/TabPractice/WordDetails/index.tsx
@@ -1,12 +1,63 @@
+import { MaterialIcons } from '@expo/vector-icons';
+import { RouteProp } from '@react-navigation/native';
+import * as Speech from 'expo-speech';
 import React, { memo } from 'react';
+import { StyleSheet } from 'react-native';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import Ripple from 'react-native-material-ripple';
 import { Text, View } from '~/components/Themed';
+import { TabPracticeParamList } from '~/types';
 
-const TabPracticeWordDetails = memo(() => {
+type Props = {
+  route: RouteProp<TabPracticeParamList, 'TabPracticeWordDetails'>;
+};
+
+const TabPracticeWordDetails = memo(({ route }: Props) => {
+  const { name, mean, explain, pronounce } = route.params.word;
+
   return (
-    <View>
-      <Text>test</Text>
+    <View style={styles.container}>
+      <Text weight={700} style={styles.name}>
+        {name}
+      </Text>
+      <Text weight={600} style={styles.pronounce}>
+        {pronounce}
+      </Text>
+      <Ripple
+        rippleCentered
+        rippleContainerBorderRadius={50}
+        style={{ padding: 3 }}
+        onPress={() => Speech.speak(name, { language: 'en' })}
+      >
+        <MaterialIcons name="volume-up" size={16} color="black" />
+      </Ripple>
+      <Text weight={400} style={styles.mean}>
+        {explain}
+      </Text>
+      <Text weight={600} style={[styles.mean, { marginTop: 20 }]}>
+        {mean}
+      </Text>
     </View>
   );
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    paddingVertical: 100,
+    paddingHorizontal: 70,
+  },
+  name: {
+    fontSize: 20,
+  },
+  pronounce: {
+    color: '#5e72e4',
+  },
+  mean: {
+    textAlign: 'center',
+  },
 });
 
 export default TabPracticeWordDetails;
---
+++ developer/ScrollView/SnapObjectsScrollSlide.md
@@ -0,0 +1,2 @@
+https://youtu.be/rWwz9WO-hCo
+horizontal decelerationRate="fast" snapToInterval={Dimensions.get('window').width}
